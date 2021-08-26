de681f2624cffdc47db29b393b223319f9e4be9b
Giovani <giovanimcorrea18@gmail.com>
2021-08-26 12:53:13 -0300

    creating modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import logoImg from "../../assets/logo.png";
+import Modal from "react-modal";
 import { Container } from "./styles";
 import { Content } from "./styles";
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
-        <a href="#"> <img className="logo" src={logoImg} alt="gMoney" /></a>
-        <button type="button">Nova transação</button>
+        <a href="#">
+          {" "}
+          <img className="logo" src={logoImg} alt="gMoney" />
+        </a>
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>
       </Content>
     </Container>
   );
